903e71dbb9b20af04db050b22ec0bf2bc4058d4c
sosi <soysal.tan@emlak24.com>
2021-11-08 13:08:49 +0300

    Add components

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import './Components/Table'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {Container, Row} from "react-bootstrap";
+import SearchForm from "./Components/SearchForm";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <>
+        <Container>
+          <Row>
+            <SearchForm/>
+          </Row>
+
+        </Container>
+      </>
   );
 }
 
